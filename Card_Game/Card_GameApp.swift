6a48b00efd88bc37ea9ab3a9b9990dee186ad587
Nearezz <biswasindranil019>
2024-04-11 09:06:52 -0400

    Initial Commit

---
+++ Card_Game/Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Card_GameApp.swift
+//  Card_Game
+//
+//  Created by NYCDOE on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
