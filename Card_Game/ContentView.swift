18c958f58bc2d4a85bc5415d62fea3e9835115a1
Nearezz <biswasindranil019>
2024-04-17 09:21:48 -0400

    Done with lazarus task

--- Card_Game/ContentView.swift
+++ Card_Game/ContentView.swift
@@ -10,20 +10,31 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Color(.green).ignoresSafeArea()
+            Color(red: 0.1, green: 0.50, blue:0.32 ).ignoresSafeArea()
             VStack {
+                Image("Logo")
+                    .padding(.bottom,28.0)
                 HStack {
                     Image("clubs_ace").resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(width: 150,height: 200, alignment: .topLeading)
+                        
+                        
+                    
                     Image("spades_king").resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(width: 150,height: 200, alignment: .topLeading)
                 }
                 
                 Text("21! A Winner")
-            }
+                
+                Button("Deal") {}
+                    .padding(.top,16.0)
+                    .buttonStyle(.borderedProminent)
+                
+                }
             .padding()
+        
         }
     }
 }
